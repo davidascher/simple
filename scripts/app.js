58ddb4d01d01c75db0e4954bc954dd0ab9084ca0
David Ascher <da@mozillafoundation.org>
2015-02-15 10:37:38 -0800

    split up editor and editable

--- scripts/app.js
+++ scripts/app.js
@@ -2,18 +2,55 @@
 
 var React = require('react');
 // var Markdown = require("./markdownfile.jsx");
-var Editable = require("./editable.jsx");
+var {Editable, Editor} = require("./editable.jsx");
 var content = "Content";
+var markdown = require('markdown').markdown;
+
 require("bootstrap/dist/css/bootstrap.min.css");
 require("bootstrap/dist/css/bootstrap-theme.min.css");
 
 var App = React.createClass({
+  getInitialState: function() {
+    return {
+      editing: false,
+      editable: {
+        raw:""
+      }
+    }
+  },
+  editEditable: function(editable, text) {
+    this.setState({
+      editing: true,
+      editable:editable,
+      text:text
+    })
+    console.log(text.slice(0,40));
+  },
+  textChanged: function(value) {
+    this.setState({ text: markdown.toHTML(value) })
+    // Figure out state management here.
+  },
+  dismissEditor: function(value) {
+    this.setState({editing:false});
+    // Figure out state management here.
+  },
+  onKeyDown: function(event) {
+    console.log("EVENT", event);
+    if (event.keyCode === 13) {
+      this.props.dismissEditor(event);
+    }
+  },
   render() {
+    var self = this;
     return (
       <div className="container">
-        <button className="btn btn-primary">foo</button>
-        <h1>Hello, world.</h1>
-        <Editable fileName="heartbeats.md"/>
+        <Editable app={self} 
+                  text={this.state.text} 
+                  fileName="heartbeats.md"/>
+        <Editor visible={this.state.editing} 
+                text={this.state.text} 
+                textChanged={this.textChanged}
+                editable={this.state.editable}/>
       </div>
     );
   }
--- scripts/editable.jsx
+++ scripts/editable.jsx
@@ -24,12 +24,11 @@ var Editor = React.createClass({
     return {blurred:false};
   },
   onChange: function(event) {
-    //
+    this.setState({text: event.target.value});
+    this.props.textChanged(event.target.value);
   },
-  onKeyUp: function(event) {
-    console.log("got onkeyup");
-    console.log(event.target.value);
-    this.props.onchange(event.target.value);
+  onKeyPress: function(event) {
+    this.props.textChanged(event.target.value);
   },
   onMouseDown: function(event) {
 
@@ -50,30 +49,27 @@ var Editor = React.createClass({
       display: this.props.visible == true ? "block" : "none",
     }
     var classes = "editor-container ";
-    if (this.state.blurred) {
-      classes = classes + "blurred";
-    } else {
+    if (this.props.visible) {
       classes = classes + "active";
+    } else {
+      classes = classes + "collapsed";
     }
+    var text = this.state.text || this.props.text;
     return (
-        <Draggable handle=".toolbar">
-          <div className={classes} onBlur={this.onBlur} onFocus={this.onFocus}>
-            <div style={editorStyle} className="editor">
-              <div className="editor-background"/>
-              <div className="toolbar" onMouseDown={this.onMouseDown}>
-                <EditorButton icon="header"/>
-                <EditorButton icon="bold"/>
-                <EditorButton icon="italic"/>
-              </div>
-              <textarea defaultValue={this.props.text} 
-                        onChange={this.onChange} 
-                        onMouseUp={this.onMouseUp}
-                        onMouseMove={this.onMouseMove}
-                        onKeyUp={this.onKeyUp} 
-                        className="inputfield"></textarea>
-            </div>
+      <div className={classes}>
+        <div style={editorStyle} className="editor">
+          <div className="editor-background"/>
+          <div className="toolbar" onMouseDown={this.onMouseDown}>
+            <EditorButton icon="header"/>
+            <EditorButton icon="bold"/>
+            <EditorButton icon="italic"/>
           </div>
-        </Draggable>
+          <textarea value={text} 
+                    onChange={this.onChange} 
+                    onKeyPress={this.onKeyPress} 
+                    className="inputfield"></textarea>
+        </div>
+      </div>
     );
   }
 });
@@ -83,12 +79,15 @@ var Editable = React.createClass({
     var data = require("../content/"+this.props.fileName);
     return {
       raw: data,
-      md:markdown.toHTML(data),
+      html: markdown.toHTML(data),
       editing: false
     }
   },
   toggleEditor: function() {
-    this.setState({'editing': ! this.state.editing });
+    console.log(this.props);
+    // let the editor know the text, and to become visible
+    this.props.app.editEditable(this, this.state.raw);
+    // this.setState({'editing': ! this.state.editing });
   },
   updateText: function(newText) {
     this.setState({
@@ -97,17 +96,18 @@ var Editable = React.createClass({
     });
   },
   render: function() {
-    var editor;
+    var editor, html;
+    if (this.props.text) {
+      html = this.props.text;
+    } else {
+      html = this.state.html;
+    }
     var contents = React.DOM.span({
-     dangerouslySetInnerHTML: {__html: this.state.md}
+     dangerouslySetInnerHTML: {__html: html}
     });
     var self = this;
     return (
       <div>
-        <Editor visible={this.state.editing} 
-                text={this.state.raw}
-                onchange={self.updateText}
-                />
         <div className="editable">
           <div onClick={this.toggleEditor} className="button edit-button">
             <i className="fa fa-2x fa-pencil-square-o"></i>
@@ -121,4 +121,5 @@ var Editable = React.createClass({
   }
 });
 
-module.exports = Editable;
+module.exports.Editable = Editable;
+module.exports.Editor = Editor;
