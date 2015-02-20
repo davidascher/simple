cba98bb5fd21d545165ef1dc1ffacacea35860d0
David Ascher <da@mozillafoundation.org>
2015-02-19 22:40:10 -0800

    let's try to get a production config

--- server.js
+++ server.js
@@ -2,13 +2,28 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true
-}).listen(process.env.PORT || 3000, 'localhost', function (err, result) {
-  if (err) {
-    console.log(err);
-  }
+if (process.env.NODE_ENV === "heroku") {
+  var serveStatic = require('serve-static');
+  var finalhandler = require('finalhandler')
+  var http = require('http')
+  var path = require('path')
+  var cwd = path.dirname(__filename);
+  var p = cwd+config.output.publicPath;
+  var express = require('express')
 
-  console.log('Listening at localhost:3000');
-});
+  var app = express()
+  app.use(serveStatic('.', {'index': ['index.html']}))
+  app.listen(process.env.PORT || 3000)
+} else {
+  new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true
+  }).listen(process.env.PORT || 3000, 'localhost', function (err, result) {
+    if (err) {
+      console.log(err);
+    }
+
+    console.log('Listening at localhost:3000');
+  });
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,61 @@
 var webpack = require('webpack');
+var port = process.env.PORT || 3000;
+
+var plugins = [
+  new webpack.NoErrorsPlugin()
+];
+var entry = [];
+var loaders = [
+  { test: /\.md$/, loaders: ['raw']},
+  { test: /\.woff2$/, loaders: ['file']},
+  { test: /\.woff$/, loaders: ['file']},
+  { test: /\.eot$/, loaders: ['file']},
+  { test: /\.ttf$/, loaders: ['file']},
+  { test: /\.svg$/, loaders: ['file']},
+  { test: /\.png$/, loaders: ['file']},
+  { test: /\.gif$/, loaders: ['file']},
+  { test: /\.css$/, loader: 'style-loader!css-loader'},
+  { test: /\.less$/, loader: "style-loader!css-loader!less-loader"}
+]
+
+if (process.env.NODE_ENV != "heroku") {
+  plugins.push(new webpack.HotModuleReplacementPlugin())
+  entry = [
+    'webpack-dev-server/client?http://localhost:'+port,
+    'webpack/hot/only-dev-server',
+    './scripts/index'
+  ];
+  loaders.push(
+    { test: /\.js$/, loaders: ['react-hot', 'jsx?harmony'], exclude: /node_modules/ }
+  );
+  loaders.push(
+    { test: /\.jsx$/, loaders: ['react-hot', 'jsx?harmony'], exclude: /node_modules/ }
+  );
+} else {
+  entry = [
+    './scripts/index'
+  ];
+  loaders.push(
+    { test: /\.js$/, loaders: ['jsx?harmony'], exclude: /node_modules/ }
+    );
+  loaders.push(
+      { test: /\.jsx$/, loaders: ['jsx?harmony'], exclude: /node_modules/ }
+    );
+}
 
 module.exports = {
   devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './scripts/index'
-  ],
+  entry: entry,
   output: {
     path: __dirname + '/scripts/',
     filename: 'bundle.js',
     publicPath: '/scripts/'
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
+  plugins: plugins,
   resolve: {
     extensions: ['', '.js', '.md']
   },
   module: {
-    loaders: [
-      { test: /\.md$/, loaders: ['raw']},
-      { test: /\.woff2$/, loaders: ['file']},
-      { test: /\.woff$/, loaders: ['file']},
-      { test: /\.eot$/, loaders: ['file']},
-      { test: /\.ttf$/, loaders: ['file']},
-      { test: /\.svg$/, loaders: ['file']},
-      { test: /\.png$/, loaders: ['file']},
-      { test: /\.gif$/, loaders: ['file']},
-      { test: /\.css$/, loader: 'style-loader!css-loader'},
-      { test: /\.less$/, loader: "style-loader!css-loader!less-loader"},
-      { test: /\.js$/, loaders: ['react-hot', 'jsx?harmony'], exclude: /node_modules/ },
-      { test: /\.jsx$/, loaders: ['react-hot', 'jsx?harmony'], exclude: /node_modules/ },
-    ]
+    loaders: loaders
   }
 };
